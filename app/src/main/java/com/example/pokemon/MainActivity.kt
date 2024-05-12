0f7d832e47d26e3882dc5a248cf93052870006a7
Anastasia745 <agnibeda99@gmail.com>
2024-05-12 22:34:39 +0300

    delete imports

--- app/src/main/java/com/example/pokemon/MainActivity.kt
+++ app/src/main/java/com/example/pokemon/MainActivity.kt
@@ -10,15 +10,13 @@ import com.squareup.picasso.Picasso
 import org.json.JSONArray
 import org.json.JSONObject
 import java.io.BufferedReader
-import java.io.IOException
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
-import java.net.MalformedURLException
 import java.net.URL
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var btn: Button
+
     private lateinit var recyclerView: RecyclerView
     private lateinit var pokemonList: ArrayList<Pokemon>
 
